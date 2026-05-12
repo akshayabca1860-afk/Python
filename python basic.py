f2df7dbf1b671bc396454c85b06b3efdadd881bd
Akshaya <akshayabca1860@gmail.com>
2026-05-12 12:25:18 +0530

    First commit

---
+++ python basic.py
@@ -0,0 +1,80 @@
+#Variables:
+#A variable is used to store a data/value like a container.
+#Syntax: variables_name=value.
+#Program:
+name="Akshu"
+age=19
+color="Green"
+city="Pochampalli"
+print(name)
+print(age)
+print(color)
+print(city)
+#Data Types:
+#DataTypes means the type of value stored in a variable. Python automatically understand the type.
+#Types of DataTypes:
+#integer data type
+#string data type
+#float data type
+#boolean data type
+#Program
+a=89
+b="Akshu"
+c=True
+d=8.90
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+#Operators
+#It is used to perform operations on values or variables.
+#Types of operators
+#Arithmetic operators
+#Assignment operators
+#Comparison operators
+#Logical operators
+#Bitwise operators
+#Membership operators
+#Identity operators
+#Program
+a=10
+b=20
+print("Addtion:",a+b)
+print("Subraction:",a-b)
+print("Multiplication:",a*b)
+print("Division:",a/b)
+print("Floor division:",a//b)
+print("Modulu:",a%b)
+c=5
+d=7
+print("Greater than:",c>b)
+print("less than:",c<d)
+print("Equal to:",c==d)
+print("Not Equal to:",c!=d)
+print("Greater than equal to",c>=d)
+print("less than equal to:",c<=d)
+x=True
+y=False
+print("x and y:",x and y)
+print("x or y:", x or y)
+print("~x:",~x)
+#Functions:
+#A function is a block of code which only run when it is called.
+def name():
+    print("Akshu")
+name()
+#add function
+def add(a,b):
+    print(a+b)
+add(5,9)
+#multiplication function
+def multiple(x,y):
+    print(x*y)
+multiple(5,3)
+#greeting function
+def greet():
+    print("Welcome")
+greet() 
+greet()
+greet()
+greet()  
