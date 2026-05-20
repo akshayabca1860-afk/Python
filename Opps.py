de3522d9f92baae73efdacc952c01649c70b684a
Akshaya <akshayabca1860@gmail.com>
2026-05-20 14:38:34 +0530

    initial python

--- Opps.py
+++ Opps.py
@@ -14,3 +14,36 @@ b=int(input("Enter your age:"))
 c=int(input("Enter your mark:"))
 s1=Student(a,b,c)
 s1.display()
+
+#Create a BankAccount class with deposit and withdraw methods.
+class BankAccount:
+    
+    def __init__(self,balance):
+        self.balance=balance
+    
+    def deposit(self,amount):
+        self.balance=self.balance+amount
+        print("Deposit Amount:",amount)
+        print("Balance:",self.balance)
+
+    def withdraw(self, amount):
+        if amount<=self.balance:
+            self.balance=self.balance-amount
+        
+            print("Withdraw Amount:",amount)
+            print("Balance:",self.balance)
+
+        else:
+            print("Insufficient Balance")
+
+
+balance=1000
+b1=BankAccount(balance)     
+
+
+deposit_amount=int(input("Enter your amount:"))
+b1.deposit(deposit_amount)
+withdraw_amount=int(input("Enter your amount:"))
+b1.withdraw(withdraw_amount)
+
+
