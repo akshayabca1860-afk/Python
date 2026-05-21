93f12385bc1d453c17d58e8f796a745de6581ee8
Akshaya <akshayabca1860@gmail.com>
2026-05-21 11:13:42 +0530

    initial python

--- Opps.py
+++ Opps.py
@@ -46,4 +46,37 @@ b1.deposit(deposit_amount)
 withdraw_amount=int(input("Enter your amount:"))
 b1.withdraw(withdraw_amount)
 
+#Create an Employee class and calculate salary.
+class Employee:
+    def __init__(self,name,salary,days):
+        self.name=name
+        self.salary=salary
+        self.days=days
+    def calculatesalary(self):
+        total_salary=self.salary*self.days
+        print("Name:",self.name)
+        print("Salary:",self.salary)
+        print("Days:",self.days)
+        print("total_salary:",total_salary)
+
+name=input("Enter the Employee name:")
+salary=int(input("Enter the Salary: "))
+days=int(input("Enter the days:"))
+e1=Employee(name,salary,days)
+e1.calculatesalary()
+
+#Use method overriding with parent and child classes.
+("\nOverriding")
+class Parent:
+    def show(self):
+        print("This is Parent class")
+class Child(Parent):
+    def show(self):
+        print("This is Child class")
+
+c1=Child()
+c1.show()
+   
+
+
 
