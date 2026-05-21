ec0f2cbe5838c50e0d5ce441a1e6148419a5021f
Akshaya <akshayabca1860@gmail.com>
2026-05-21 12:15:33 +0530

    initial python

---
+++ Collections.py
@@ -0,0 +1,19 @@
+#Store student names in using arraylist
+student=[] 
+n=int(input("Enter the number of students:"))
+for i in range(n):
+    name=input("Enter the students name:")
+    student.append(name)
+print("Students name:")
+for name in student:
+    print(name)
+
+#Remove duplicate numbers using HashSet
+("\nDuplicate")
+numbers=set()
+n=int(input("Enter the number of elements:"))
+for i in range(n):
+    num=int(input("Enter the number:"))
+    numbers.add(num)
+print("After removing the duplicates:")
+print(numbers)
